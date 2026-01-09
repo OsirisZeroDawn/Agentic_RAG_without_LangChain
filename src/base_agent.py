cb37835f28182185734ff0733611c6f8bae3f913
A <ashley@evatt.ai>
2026-01-09 11:56:54 +0800

    adding basics of open ai & base agent

--- src/base_agent.py
+++ src/base_agent.py
@@ -10,5 +10,18 @@ class BaseAgent:
 
     def run(self,task: str, context:dict)
         messages = [
-            {"role":"system","content":self.system_prompt}
+            {"role":"system","content":self.system_prompt},
+            {
+                "role":"user"
+                "content":f"""Task:{task}
+                 Context:{context}
+            """
+            }
         ]
+
+        response = client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=messages
+        )
+
+        return response.choices[0].message.content
