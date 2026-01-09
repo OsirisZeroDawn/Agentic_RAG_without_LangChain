6c41a471f7426c172983102954f766e983d8b612
A <ashley@evatt.ai>
2026-01-09 13:49:19 +0800

    adding the relevant subtasks

--- src/main.py
+++ src/main.py
@@ -15,5 +15,18 @@ class IdeaInput(BaseModel):
         return orchestrator.run(input.ideas)
 
 """
+Sub-Tasks Definition
+
+1- User Interface Development
+Design and implement a simple web-based user interface using streamlit or Gradio.
+
+2- Prompt Engineering
+Create a refine structured prompt for each AI agetn to ensure clear reasoning, role seperation.
+
+3- Structure Output Definition
+Define the expected AI outputs according to business requirements, specifying dimensions such as industry, competitors, oppurtunities, and bling spots.
+
+4- Dashboard
+Build a simple dashboard that transforms the structured AI outputs into clear tables and charts
 
 """
