e699da4a2d2518eb7b701cd4ef5cecf25316a29c
A <ashley@evatt.ai>
2026-01-09 13:43:37 +0800

    creating the main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,19 @@
-from fastapi
+from fastapi import FastAPI
+from pydantic import BaseModel
+from orchestrator import GalaxonOrchestrator
+
+app = FastAPI()
+orchestrator = GalaxonOrchestrator()
+
+
+class IdeaInput(BaseModel):
+    idea: str
+
+
+    @app.post("/analyze")
+    def analyze (input: IdeaInput):
+        return orchestrator.run(input.ideas)
+
+"""
+
+"""
